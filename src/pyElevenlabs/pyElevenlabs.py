37a1c2f5a7c6d1025f710f8b7262e5bc3fadeba5
Philip Lawall <philip.lawall@qvest.com>
2023-02-10 13:09:20 +0100

    first working example

--- src/pyElevenlabs/pyElevenlabs.py
+++ src/pyElevenlabs/pyElevenlabs.py
@@ -9,6 +9,21 @@ SpeechUrlTemplate = "https://api.elevenlabs.io/v1/text-to-speech/{}"
 class pyElevenlabs(object):
     def __init__(self, apiKey):
         self._apiKey = apiKey
+        self._headers = {"xi-api-key": self._apiKey}
 
-        headers = {"xi-api-key": self._apiKey}
         _LOGGER.debug("Connection Initialized OK")
+
+    def speak(self, voice_id, text):
+        _LOGGER.debug("Speak: %s", text)
+
+        url = SpeechUrlTemplate.format(voice_id)
+        _LOGGER.debug("URL: %s", url)
+
+        response = requests.post(
+            url, headers=self._headers, json={"text": text})
+        _LOGGER.debug("Response: %s", response)
+
+        if response.ok:
+            return response.content
+        else:
+            raise Exception(response.text)
--- src/pyElevenlabs/test.py
+++ src/pyElevenlabs/test.py
@@ -1,9 +1,19 @@
+import os
 from pyElevenlabs import pyElevenlabs
 from dotenv import load_dotenv
 
 load_dotenv()
 
-api_key = ""
+api_key = os.getenv("API_KEY")
+
+print(api_key)
 
 t = pyElevenlabs(api_key)
-data = t.speak("Hello World!")
+data = t.speak("21m00Tcm4TlvDq8ikWAM",
+               "This is a realy long text to test the api.")
+
+with open("test.wav", "wb") as f:
+    f.write(data)
+
+# play audio file
+os.system("test.wav")
