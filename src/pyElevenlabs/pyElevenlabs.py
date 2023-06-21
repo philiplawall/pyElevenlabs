16cf57b83a1a2e4f63bd5682ee0669f041de9573
Philip Lawall <philiplawall@gmail.com>
2023-06-21 21:05:31 +0200

    add model_id

--- src/pyElevenlabs/pyElevenlabs.py
+++ src/pyElevenlabs/pyElevenlabs.py
@@ -8,11 +8,11 @@ class pyElevenlabsClient(object):
         self._apiKey = apiKey
         self._headers = {"xi-api-key": self._apiKey}
 
-    def speak(self, voice_id, text):
+    def speak(self, voice_id, text, model="eleven_monolingual_v1"):
         url = SpeechUrlTemplate.format(voice_id)
 
         response = requests.post(
-            url, headers=self._headers, json={"text": text})
+            url, headers=self._headers, json={"text": text, "model_id": model})
 
         response.raise_for_status()
         return response.content
